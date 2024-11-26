afc4dbee00a2f3f7ea7e85eb9e343ab1fbb317c8
Jeffrey Mooiweer <33760627+jeffreymooiweer@users.noreply.github.com>
2024-11-26 14:53:12 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///backend/devices.db'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(BASE_DIR, "devices.db")}'
 db = SQLAlchemy(app)
 
 class Device(db.Model):
